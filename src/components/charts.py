20d7f9ac8de74c504f3ab1324fdb5764af48c50f
Vitoria Maria <vitoriamaria.sds@gmail.com>
2025-04-09 11:55:00 -0300

    refactor: melhorar tratamento de credenciais BigQuery

--- src/components/charts.py
+++ src/components/charts.py
@@ -121,4 +121,3 @@ def create_heatmap(
 def render_chart(fig: go.Figure, key: Optional[str] = None) -> None:
     st.plotly_chart(fig, use_container_width=True, key=key)
     logger.debug("Grafico renderizado")
-
